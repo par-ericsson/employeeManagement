b4655a7e3c1737170f5c6d26c7c11346b48a7344
Pär Ericsson <ericsson@home.se>
2020-07-21 20:34:51 +0200

    Added AdminLTE

--- employeeManagement/Controllers/HomeController.cs
+++ employeeManagement/Controllers/HomeController.cs
@@ -6,9 +6,11 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using employeeManagement.Models;
+using Microsoft.AspNetCore.Authorization;
 
 namespace employeeManagement.Controllers
 {
+    [Authorize]
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
